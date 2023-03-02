40048f2da7ae3c9e329b87a5894558aa3c8e1b28
PiotrKukula0 <piotr.kukula@student.po.edu.pl>
2023-03-02 18:48:49 +0100

    styling page 1 and finishing page 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Section = styled.div`
   display: flex;
   flex-direction:column;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
 `
 const Container = styled.div`
   height: 100vh;
@@ -18,31 +18,63 @@ const Container = styled.div`
   justify-content: space-between;
 `
 const Left = styled.div`
+  flex:2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
 
 `
 const Title = styled.h1`
-
+  font-size: 74px;
 `
 const WhatWeDo = styled.div`
-
+  display: flex;
+  align-items:center;
+  gap: 10px;
 `
 const Line = styled.img`
-
+  height:5px;
 `
 const Subtitle = styled.h2`
-
+  color: #da4ea2
 `
 const Desc = styled.p`
-
+  font-size: 24px;
+  color: lightgrey;
 `
 const Button = styled.div`
-
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
+  text-align: center;
+  width: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 `
 const Right = styled.div`
-
+  flex:3;
+  position: relative;
 `
 const Img = styled.img`
+  width: 800px;
+  height: 600px;
+  object-fit: contain;
+  position: absolute;
+  top:0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  animation: animate 2s infinite ease alternate;
 
+  @keyframes animate {
+    to{
+      transform: translateY(20px)
+    }
+  }
 `
 
 
@@ -56,8 +88,8 @@ const Hero = () => {
       Think. Make. Solve.
       </Title>
       <WhatWeDo>
-        <Line src=".img/line.png"/>
-        <Subtitle></Subtitle>
+        <Line src="./img/line.png"/>
+        <Subtitle>What we Do</Subtitle>
       </WhatWeDo>
       <Desc>
         we enjoy creating delightful, human-created digital experiences
@@ -65,6 +97,7 @@ const Hero = () => {
       <Button>Learn more</Button>
       </Left>
       <Right>
+      <Img src='./img/moon.png'/>
        </Right>
        </Container>
     </Section>
