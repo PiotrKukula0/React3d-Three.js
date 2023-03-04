a790731c1f5e5bd7f681048751c0272095ce7714
PiotrKukula0 <piotr.kukula@student.po.edu.pl>
2023-03-04 19:12:46 +0100

    Installing Three.js and adding a 3D cube

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -1,84 +1,112 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { Suspense } from "react";
+import styled from "styled-components";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import Cube from "./Cube";
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
   display: flex;
   justify-content: center;
-`
+`;
+
 const Container = styled.div`
   height: 100vh;
   scroll-snap-align: center;
   width: 1400px;
   display: flex;
   justify-content: space-between;
-`
-const Left = styled.div`
-  flex:1;
+`;
 
-`
+const Left = styled.div`
+  flex: 1;
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 60px;
+  }
+`;
 
 const Right = styled.div`
-  flex:1;
+  flex: 1;
   display: flex;
   flex-direction: column;
   justify-content: center;
   gap: 20px;
-`
 
-const Title = styled.h1`
-  font-size: 74px;
-`
+  @media only screen and (max-width: 768px) {
+    align-items: center;
+    text-align: center;
+  }
+`;
+
 const WhatWeDo = styled.div`
   display: flex;
-  align-items:center;
+  align-items: center;
   gap: 10px;
-`
+`;
+
 const Line = styled.img`
-  height:5px;
-`
+  height: 5px;
+`;
+
 const Subtitle = styled.h2`
-  color: #da4ea2
-`
+  color: #da4ea2;
+`;
+
 const Desc = styled.p`
   font-size: 24px;
-  color: lightgrey;
-`
-const Button = styled.div`
+  color: lightgray;
+`;
+
+const Button = styled.button`
   background-color: #da4ea2;
   color: white;
   font-weight: 500;
-  text-align: center;
   width: 120px;
   padding: 10px;
   border: none;
   border-radius: 5px;
   cursor: pointer;
-`
+`;
 
 const Who = () => {
   return (
     <Section>
       <Container>
-      <Left>
-      </Left>
-      <Right>
-      <Title>
-      Think outside the square space
-      </Title>
-      <WhatWeDo>
-        <Line src="./img/line.png"/>
-        <Subtitle>Who we are</Subtitle>
-      </WhatWeDo>
-      <Desc>
-        a creative group of designers and developers with passion for the arts.
-      </Desc>
-      <Button>See our works</Button>
-       </Right>
-       </Container>
+        <Left>
+          <Canvas camera={{ position: [5, 5, 5], fov: 25 }}>
+            <Suspense fallback={null}>
+              <ambientLight intensity={0.5} />
+              <directionalLight position={[3, 2, 1]} />
+              <Cube />
+              <OrbitControls enableZoom={false} autoRotate />
+            </Suspense>
+          </Canvas>
+        </Left>
+        <Right>
+          <Title>Think outside the square space</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <Subtitle>Who we Are</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            a creative group of designers and developers with a passion for the
+            arts.
+          </Desc>
+          <Button>See our works</Button>
+        </Right>
+      </Container>
     </Section>
-  )
-}
+  );
+};
 
-export default Who
+export default Who;
